88e1fdea80dc4e361280fa4056d53671ca0cafa0
Las <las@protonmail.ch>
2017-11-14 17:24:32 +0100

    Make laspad mod

--- output/lua/FS_Client.lua
+++
@@ -1,4 +0,0 @@
-Script.Load("lua/Client.lua")
-Script.Load("lua/FS_Shared.lua")
-
-Script.Load("lua/FS_MarineTechMap.lua")
--- output/lua/FS_MarineTeam.lua
+++
@@ -1,9 +0,0 @@
-
-Script.Load("lua/MarineTeam.lua")
-
-local MarineTeamInitTechTree = MarineTeam.InitTechTree
-function MarineTeam:InitTechTree()
-   MarineTeamInitTechTree(self)
-   self.techTree:AddBuildNode(kTechId.FlameSentry, kTechId.RoboticsFactory, kTechId.None, true)
-   self.techTree:SetComplete()
-end
