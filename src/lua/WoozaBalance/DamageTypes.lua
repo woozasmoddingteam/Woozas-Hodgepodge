6e27e913b7014ce584a416b2a31ec7c1db41f655
Las <las@protonmail.ch>
2019-03-29 19:48:04 +0100

    Properly revert Grenade Launcher damage reversion

--- src/lua/WoozaBalance/DamageTypes.lua
+++ src/lua/WoozaBalance/DamageTypes.lua
@@ -1,1 +1,4 @@
-kGLStructuralDamageScalar = (145 * 2) / 90
+if not kGLStructuralDamageScalar then
+	Shared.Message "\n\n\n\n kGLStructuralDamageScalar not found\n\n\n\n"
+end
+kGLStructuralDamageScalar = (165 * 2) / 90
